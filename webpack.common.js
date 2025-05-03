e064a29dfc8176f77cfb44634f70daaac754d174
Nivesh <niveshjain2012@gmail.com>
2025-05-03 12:12:46 +0530

    Env specific configs

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: {
     main: "./src/main.js",
-  },
-  output: {
-    filename: "main.[hash].js",
-    path: path.resolve(__dirname, "dist"),
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
   module: {
